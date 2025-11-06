42fa9371955d3d5c632ac6d921f2aed23983129c
Abbas <abbas.h.8215@gmail.com>
2025-11-06 13:51:10 +0300

    SinUp is ready

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_getit_supabase_project_abdualaziz_abbas_abdulaziz/features/login/pages/login_page.dart';
+import 'package:bloc_getit_supabase_project_abdualaziz_abbas_abdulaziz/features/sinup.dart/pages/signup_page.dart';
 import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 
@@ -9,6 +10,11 @@ final GoRouter router = GoRouter(
       path: '/login',
       name: 'login',
       builder: (BuildContext context, GoRouterState state) => const LoginPage(),
+    ),
+    GoRoute(
+      path: '/sign_up',
+      name: 'sign_up',
+      builder: (context, state) => const SignUpPage(),
     ),
   ], //GoRoute(path: '/sign_in', builder: LoginPage.builder)
 );
---
+++ lib/features/sinup.dart/pages/signup_page.dart
@@ -0,0 +1,159 @@
+// lib/features/auth/signup/ui/signup_page.dart
+
+import 'package:bloc_getit_supabase_project_abdualaziz_abbas_abdulaziz/core/di/get_it.dart';
+import 'package:bloc_getit_supabase_project_abdualaziz_abbas_abdulaziz/features/login/widgets/custom_text_field.dart';
+import 'package:bloc_getit_supabase_project_abdualaziz_abbas_abdulaziz/features/sinup.dart/bloc/sinup_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:bloc_getit_supabase_project_abdualaziz_abbas_abdulaziz/core/services/auth_service.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => SignupBloc(authService: locator<AuthService>()),
+      child: const _SignUpView(),
+    );
+  }
+}
+
+class _SignUpView extends StatelessWidget {
+  const _SignUpView();
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = context.read<SignupBloc>();
+
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: SingleChildScrollView(
+            child: BlocListener<SignupBloc, SignupState>(
+              listener: (context, state) {
+                if (state.success) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Account created successfully!'),
+                    ),
+                  );
+                  context.go('/login');
+                } else if (state.errorMessage != null) {
+                  ScaffoldMessenger.of(
+                    context,
+                  ).showSnackBar(SnackBar(content: Text(state.errorMessage!)));
+                }
+              },
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 80),
+                  const Center(
+                    child: Text(
+                      'Create your account',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+
+                  //  Email
+                  const Text('Email Address'),
+                  const SizedBox(height: 8),
+                  BlocBuilder<SignupBloc, SignupState>(
+                    builder: (context, state) {
+                      return CustomTextField(
+                        hintText: 'you@email.com',
+                        icon: Icons.email,
+                        onChanged: (value) => bloc.add(EmailChanged(value)),
+                      );
+                    },
+                  ),
+                  const SizedBox(height: 20),
+
+                  // Password
+                  const Text('Password'),
+                  const SizedBox(height: 8),
+                  BlocBuilder<SignupBloc, SignupState>(
+                    builder: (context, state) {
+                      return CustomTextField(
+                        hintText: 'Enter your password',
+                        icon: Icons.lock_outline,
+                        onChanged: (value) => bloc.add(PasswordChanged(value)),
+                        obscureText: true,
+                      );
+                    },
+                  ),
+                  const SizedBox(height: 20),
+
+                  //  Confirm Password
+                  const Text('Confirm Password'),
+                  const SizedBox(height: 8),
+                  BlocBuilder<SignupBloc, SignupState>(
+                    builder: (context, state) {
+                      return CustomTextField(
+                        hintText: 'Re-enter your password',
+                        obscureText: true,
+
+                        icon: Icons.lock_outline,
+                        onChanged: (value) =>
+                            bloc.add(ConfirmPasswordChanged(value)),
+                      );
+                    },
+                  ),
+
+                  const SizedBox(height: 24),
+
+                  //  Sign Up Button
+                  BlocBuilder<SignupBloc, SignupState>(
+                    builder: (context, state) {
+                      return SizedBox(
+                        width: double.infinity,
+                        height: 48,
+                        child: ElevatedButton(
+                          onPressed: state.isValid
+                              ? () => bloc.add(const SignupSubmitted())
+                              : null,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            foregroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          child: state.isLoading
+                              ? const CircularProgressIndicator(
+                                  color: Colors.white,
+                                )
+                              : const Text('Sign Up'),
+                        ),
+                      );
+                    },
+                  ),
+
+                  const SizedBox(height: 30),
+
+                  //  Go to login page
+                  Center(
+                    child: TextButton(
+                      onPressed: () => context.go('/login'),
+                      child: const Text(
+                        'Already have an account? Log in',
+                        style: TextStyle(color: Colors.black87),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
