cad71c6d51797dc48641bb1fbb67d8d2221f70ec
Abbas <abbas.h.8215@gmail.com>
2025-11-07 10:12:55 +0300

    Addind name validator_Editing the validation file

--- lib/core/utils/validators.dart
+++ lib/core/utils/validators.dart
@@ -1,4 +1,14 @@
 class Validators {
+  static String? validateName(String? value) {
+    if (value == null || value.trim().isEmpty) {
+      return 'Please enter your name';
+    }
+    if (value.trim().length < 3) {
+      return 'Name must be at least 3 characters';
+    }
+    return null;
+  }
+
   static String? validateEmail(String? value) {
     if (value == null || value.isEmpty) {
       return 'Please enter your email';
